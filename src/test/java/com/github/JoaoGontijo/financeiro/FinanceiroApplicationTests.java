511ec18afa7c6bd96af39a93d7d314754056c729
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-08 19:12:52 -0300

    primeiro commit

---
+++ src/test/java/com/github/JoaoGontijo/financeiro/FinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.JoaoGontijo.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
